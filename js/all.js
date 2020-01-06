0c74969980a869b9f46f558d52a46e8e563f2773
SlomskisL <50462014+SlomskisL@users.noreply.github.com>
2020-01-06 22:35:55 +0000

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,11 @@
+
+    /* Hamburger Menu Script */
+    function toggleMenu(){
+        var x = document.getElementById("main-menu");
+        if(x.style.display === "block"){
+            x.style.display = "none";
+        }
+        else{
+            x.style.display = "block";
+        }
+    }
