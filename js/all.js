c627caebd5b476b42cfb4eaef7ca26b7c93e9924
SlomskisL <50462014+SlomskisL@users.noreply.github.com>
2020-11-22 20:36:40 +0000

    Hamburger fix
    Fixed automatic scroll to top on click.
    Disabled scrolling when hamburger menu overlay is active.

--- js/all.js
+++ js/all.js
@@ -1,11 +1,34 @@
 // Functions to open/close navigation menu
 function openNav(){
     document.getElementById("nav").style.width = "100%";
+    disableScrolling();
 }
 
 function closeNav(){
     document.getElementById("nav").style.width = "0%";
+    enableScrolling();
 }
 
 
 // Make navbar stay on top of site
+window.onscroll = function(){stickyMenu()};
+var header = document.getElementById("section-1");
+var sticky = header.offsetTop;
+
+function stickyMenu(){
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+}
+
+function disableScrolling(){
+  var x=window.scrollX;
+  var y=window.scrollY;
+  window.onscroll=function(){window.scrollTo(x, y);};
+}
+
+function enableScrolling(){
+  window.onscroll=function(){};
+}
